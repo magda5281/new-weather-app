1eee934069b9582cd30209d7f9003534b5bceac2
Your Name <you@example.com>
2021-04-22 14:41:03 +0100

    updated date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp){
+    //calculate the date 
+        let now = new Date (timestamp);
+
+       let weekDays = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"]
+       let day = weekDays[now.getDay()];
+
+       let date = now.getDate();
+
+       let months=["January","February", "March","April","May","June","July", "August","September","October","November","December"];
+       let month=months[now.getMonth()];
+
+       let year=now.getFullYear();
+       let hours=now.getHours();
+       if (hours<10) {
+           hours===`0${hours}`
+       }
+       let nminutes=now.getMinutes();
+       if (minutes<0) {
+           minutes===`0${hours}`
+       }    
+
+    return `${day}, ${date} ${month} ${year}, ${hours}:${minutes}` 
+}
+
+
 function displayTemperature(response){
 console.log(response.data);
 let temperatureElement=document.querySelector("#temperature");
@@ -5,11 +31,14 @@ let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
+let dateElement=document.querySelector("#date");
+
 temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML= Math.round(response.data.wind.speed)
+windElement.innerHTML= Math.round(response.data.wind.speed);
+dateElement.innerHTML= formatDate(response.data.dt*1000);
 
 }
 
