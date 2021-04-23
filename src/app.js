d53d24fd0e251b6b27073f0801aea368b73243a2
Your Name <you@example.com>
2021-04-23 12:58:16 +0100

    added links to temp units and conversion C/F

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
 let mainIconElement=document.querySelector("#main-icon");
 
+
 temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
@@ -41,6 +42,9 @@ windElement.innerHTML= Math.round(response.data.wind.speed);
 dateElement.innerHTML= formatDate(response.data.dt*1000);
 mainIconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 mainIconElement.setAttribute("alt", response.data.weather[0].description);
+
+celciusTemperature = Math.round(response.data.main.temp);
+
 }
 
 function search(city){
@@ -54,9 +58,39 @@ function handleSubmit(event) {
     event.preventDefault()
     let cityInputElement=document.querySelector("#city-input")
     search(cityInputElement.value);
+    
+}
+
+function dispalyFarenheitTemperature(event) {
+    event.preventDefault();
+    let farenheitTemperatureValue= Math.round((8 * 9/5) + 32 );
+    //remove active link to celsius link 
+    celciusLinkElement.classList.remove("active");
+    farenheitLinkElement.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = farenheitTemperatureValue;
+}
+
+function dispalyCelsiusTemperature(event){
+     event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    farenheitLinkElement.classList.remove("active");
+    celciusLinkElement.classList.add("active");
+    temperatureElement.innerHTML = celciusTemperature;
 
 }
 
 
+let celciusTemperature = null
+
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+
+let farenheitLinkElement=document.querySelector("#farenheit-link");
+farenheitLinkElement.addEventListener("click", dispalyFarenheitTemperature);
+
+let celciusLinkElement=document.querySelector("#celcius-link");
+celciusLinkElement.addEventListener("click", dispalyCelsiusTemperature);
+
+
+search("warsaw");
